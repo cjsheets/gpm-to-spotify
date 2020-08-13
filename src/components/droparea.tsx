5836f674a8a10edf9a62d8a120af27f4b9bd89ac
Chad Sheets <cjsheets@gmail.com>
2020-08-12 21:52:34 -0700

    parse csv content

--- src/components/droparea.tsx
+++ src/components/droparea.tsx
@@ -1,38 +1,74 @@
 import React, { useState } from 'react';
+import { useToasts, Loading } from '@zeit-ui/react';
+import { parse } from 'papaparse';
 
 export default function DropArea({ children }: React.PropsWithChildren<{}>) {
-  const [folderTree, setFolderTree] = useState({ Playlists: {} });
-
-  function traverseTree(fsEntry: any, path = '') {
-    if (fsEntry.isFile) {
-      fsEntry.file((file: File) => {
-        console.log('file', file.name);
-      });
-    } else if (fsEntry.isDirectory) {
-      const dirReader = fsEntry.createReader();
-      dirReader.readEntries((entries: any) => {
-        for (let i = 0; i < entries.length; i++) {
-          traverseTree(entries[i], path + fsEntry.name + '/');
-        }
-      });
-    }
-  }
+  const [isParsing, setIsParsing] = useState(false);
+  const [, setToast] = useToasts();
 
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
 
+    function traverseDirectory(entry: any) {
+      const reader = entry.createReader();
+      return new Promise((resolve, reject) => {
+        const iterationAttempts: any = [];
+
+        function readEntries() {
+          reader.readEntries(
+            (entries: any) => {
+              if (entries.length) {
+                iterationAttempts.push(
+                  Promise.all(
+                    entries.map((entry: any) => {
+                      if (entry.isFile && entry.name.indexOf('.csv') > -1) {
+                        return new Promise((res) => {
+                          const playlist = entry.fullPath.split('/')[2];
+                          entry.file((file: File) => {
+                            parse(file, {
+                              header: true,
+                              complete: (r) => {
+                                res({ [playlist]: r.data[0] });
+                              },
+                            });
+                          });
+                        });
+                      }
+
+                      return traverseDirectory(entry);
+                    })
+                  )
+                );
+
+                // Call again until all entries are returned
+                readEntries();
+              } else {
+                // Done iterating this directory
+                resolve(Promise.all(iterationAttempts));
+              }
+            },
+            (error: Error) => reject(error)
+          );
+        }
+
+        readEntries();
+      });
+    }
+
     if (event?.dataTransfer) {
       const items = event.dataTransfer.items;
       for (let i = 0; i < items.length; i++) {
-        const playlistFolder = items[i].webkitGetAsEntry();
-        if (playlistFolder && playlistFolder.name == 'Playlists') {
-          traverseTree(playlistFolder);
+        const rootFolder = items[i].webkitGetAsEntry();
+        if (rootFolder && rootFolder.name == 'Playlists') {
+          setIsParsing(true);
+          traverseDirectory(rootFolder).then((result) => {
+            setIsParsing(false);
+            console.log(result);
+          });
         } else {
-          throw new Error('Must drag and drop the "Playlists" folder');
+          setToast({ text: 'Drag in the folder called "Playlists"', type: 'error' });
         }
       }
-
-      console.log(folderTree);
     }
   };
 
@@ -44,6 +80,7 @@ export default function DropArea({ children }: React.PropsWithChildren<{}>) {
   return (
     <div onDrop={handleDrop} onDragOver={handleDragOver}>
       {children}
+      {isParsing && <Loading size="large">{'Please wait, parsing playlists'}</Loading>}
     </div>
   );
 }
